2906275e55224193b69c7afb03221eb506bfd461
Yeom-JinHo <pwlsghq@naver.com>
2025-11-03 21:51:49 +0900

    refactor: MackBooScroll src 없을때 대처

--- apps/payday-records/src/components/MacBookScroll.tsx
+++ apps/payday-records/src/components/MacBookScroll.tsx
@@ -168,12 +168,14 @@ export const Lid = ({
         className="absolute inset-0 h-96 w-[32rem] rounded-2xl bg-[#010101] p-2"
       >
         <div className="absolute inset-0 rounded-lg bg-[#272729]" />
-        <Image
-          src={src || ""}
-          alt="aceternity logo"
-          fill
-          className="absolute inset-0 h-full w-full rounded-lg object-cover object-left-top"
-        />
+        {src && (
+          <Image
+            src={src}
+            alt="macbook image"
+            fill
+            className="absolute inset-0 h-full w-full rounded-lg object-cover object-left-top"
+          />
+        )}
       </motion.div>
     </div>
   );
